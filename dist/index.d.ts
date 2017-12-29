06437046e4833d49e84365e0cf4bdb75a0eca2cd
pedsm <peumendonca@gmail.com>
2017-12-29 15:57:56 +0000

    Removed old dist folder

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export default function access(axisCode: string, data: string | object | any[] | undefined | null): void;
-export interface Token {
-    value: string;
-    index: number;
-}
-export declare function tokenize(axisCode: string): Token[];
